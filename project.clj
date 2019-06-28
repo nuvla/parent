ce0f5a192b04da6f0295b66b65e034ab6681ac4c
Charles Loomis <cal@sixsq.com>
2019-06-28 15:35:05 +0200

    bump dependencies

--- project.clj
+++ project.clj
@@ -39,7 +39,7 @@
    ;; core languages
    ;;
 
-   [org.clojure/clojure "1.10.0"]
+   [org.clojure/clojure "1.10.1"]
    [org.clojure/clojurescript "1.10.520"]
 
    ;;
@@ -55,14 +55,14 @@
    [buddy/buddy-hashers "1.3.0"]
    [buddy/buddy-sign "3.0.0"]
 
-   [cc.qbits/spandex "0.7.0-beta3"]
+   [cc.qbits/spandex "0.7.0"]
    [cheshire "5.8.1"]                                       ;; newer, explicit version needed by ring-json
    [clj-commons/secretary "1.2.4"]                          ;; patched version for clojurescript 1.10.439
-   [clj-http "3.9.1"]
+   [clj-http "3.10.0"]
    [clj-stacktrace "0.2.8"]
    [clojure.java-time "0.3.2"]
    [compojure "1.6.1"]
-   [com.amazonaws/aws-java-sdk-s3 "1.11.538"]
+   [com.amazonaws/aws-java-sdk-s3 "1.11.583"]
    [com.cemerick/url "0.1.1"
     :exclusions [com.cemerick/clojurescript.test]]
    [com.draines/postal "2.0.3"]
@@ -73,7 +73,7 @@
    [com.taoensso/timbre "4.10.0"]
 
    [day8.re-frame/http-fx "0.1.6"]
-   [duratom "0.4.2"]
+   [duratom "0.4.3"]
 
    [environ "1.1.0"]
    [expound "0.7.2"]
@@ -91,17 +91,17 @@
    [org.apache.logging.log4j/log4j-core "2.11.2"]
    [org.apache.logging.log4j/log4j-api "2.11.2"]
    [org.apache.logging.log4j/log4j-web "2.11.2"]
-   [org.apache.httpcomponents/httpclient "4.5.8"]           ; force version used by clj-http
+   [org.apache.httpcomponents/httpclient "4.5.9"]           ; force version used by clj-http
    [org.slf4j/slf4j-simple "1.7.26"]
 
    [me.raynes/fs "1.4.6"]
-   [metosin/spec-tools "0.9.1"]
+   [metosin/spec-tools "0.10.0"]
 
    [org.clojure/tools.logging "0.4.1"]
-   [org.clojure/tools.namespace "0.2.11"]
+   [org.clojure/tools.namespace "0.3.0"]
    [org.clojure/data.json "0.2.6"]
    [org.clojure/java.classpath "0.3.0"]
-   [org.clojure/core.async "0.4.490" :exclusions [org.clojure/tools.reader]]
+   [org.clojure/core.async "0.4.500" :exclusions [org.clojure/tools.reader]]
    [org.clojure/test.check "0.9.0" :scope "test"]
    [org.elasticsearch/elasticsearch ~es-version]
    [org.elasticsearch.client/transport ~es-version]
@@ -119,10 +119,10 @@
    [org.apache.curator/curator-test "2.12.0" :scope "test"]
 
    [reagent "0.8.1"]
-   [re-frame "0.10.6"]
+   [re-frame "0.10.7"]
    [ring "1.7.1"]
    [ring/ring-core "1.7.1" :exclusions [commons-io]]        ; clj-http needs a more recent version
-   [ring/ring-codec "1.1.1"]
+   [ring/ring-codec "1.1.2"]
    [ring/ring-json "0.4.0"]
    [ring/ring-defaults "0.3.2"]
 
@@ -142,14 +142,14 @@
    [clojure-complete "0.2.5" :scope "test"
     :exclusions [org.clojure/clojure]]
 
-   [com.google.javascript/closure-compiler-unshaded "v20190415"]
+   [com.google.javascript/closure-compiler-unshaded "v20190618"]
 
    ;; cljs testing; control options here
    [doo "0.1.11" :scope "test"]
 
    [peridot "0.5.1" :scope "test"]
 
-   [thheller/shadow-cljs "2.8.33"]
+   [thheller/shadow-cljs "2.8.39"]
    ]
 
   :repositories
