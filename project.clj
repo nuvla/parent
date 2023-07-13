4766ace219d263623adfe6701c230f8160ed3fe3
khaled basbous <khaled@sixsq.com>
2023-07-13 09:35:42 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -13,9 +13,9 @@
             [jonase/eastwood "1.2.2"]
             [lein-cloverage "1.2.2"]
             [com.github.clj-kondo/lein-clj-kondo "0.1.3"]
-            [lein-nvd "1.9.0"]
+            [lein-nvd "2.0.0"]
             [lein-nsorg "0.3.0"]
-            [s3-wagon-private "1.3.2"]
+            [s3-wagon-private "1.3.5"]
             [lein-shell "0.5.0"]]
 
   :filespecs [{:type :path
@@ -43,7 +43,7 @@
    ;; Do not update to clojure 1.11.0 for the moment.
    ;; This will make IDE repl fail to run tests for api-server-private.
    [org.clojure/clojure "1.10.3"]
-   [org.clojure/clojurescript "1.11.4"]
+   [org.clojure/clojurescript "1.11.60"]
 
    ;;
    ;; general dependencies
@@ -55,31 +55,31 @@
    ;; Conflicts with elasticsearch test framework
    [aleph "0.4.6"]
 
-   [buddy/buddy-core "1.10.413"]
-   [buddy/buddy-hashers "1.8.158"]
-   [buddy/buddy-sign "3.4.333"]
+   [buddy/buddy-core "1.11.423"]
+   [buddy/buddy-hashers "2.0.167"]
+   [buddy/buddy-sign "3.5.351"]
 
-   [cc.qbits/spandex "0.7.10"]
-   [cheshire "5.10.2"]
-   [clj-commons/clj-yaml "0.7.108"]
+   [cc.qbits/spandex "0.7.11"]
+   [cheshire "5.11.0"]
+   [clj-commons/clj-yaml "1.0.26"]
    [clj-commons/secretary "1.2.4"]
    [clj-http "3.12.3"]
    [clj-stacktrace "0.2.8"]
-   [clojure.java-time "0.3.3"]
-   [compojure "1.6.2"]
-   [com.amazonaws/aws-java-sdk-s3 "1.12.186"]
+   [clojure.java-time "1.2.0"]
+   [compojure "1.7.0"]
+   [com.amazonaws/aws-java-sdk-s3 "1.12.505"]
    [com.cemerick/url "0.1.1"
     :exclusions [com.cemerick/clojurescript.test]]
    [com.draines/postal "2.0.5"]
    [com.rpl/specter "1.1.4"]
    [org.clojure/tools.reader "1.3.6"]
 
-   [duratom "0.5.3"]
+   [duratom "0.5.8"]
 
    [environ "1.2.0"]
    [expound "0.9.0"]
 
-   [instaparse "1.4.10"]
+   [instaparse "1.4.12"]
    [io.nervous/kvlt "0.1.5-20180119.082733-5"
     :exclusions [org.clojure/clojurescript]]
 
@@ -89,39 +89,39 @@
                  com.sun.jdmk/jmxtools
                  com.sun.jmx/jmxri]]
 
-   [org.apache.logging.log4j/log4j-core "2.17.2"]
-   [org.apache.logging.log4j/log4j-api "2.17.2"]
-   [org.apache.logging.log4j/log4j-web "2.17.2"]
-   [org.apache.httpcomponents/httpclient "4.5.13"]          ; force version used by clj-http
+   [org.apache.logging.log4j/log4j-core "2.20.0"]
+   [org.apache.logging.log4j/log4j-api "2.20.0"]
+   [org.apache.logging.log4j/log4j-web "2.20.0"]
+   [org.apache.httpcomponents/httpclient "4.5.14"]          ; force version used by clj-http
 
    [me.raynes/fs "1.4.6"]
    [metosin/spec-tools "0.10.5"]
 
    [org.clojure/tools.logging "1.2.4"]
-   [org.clojure/tools.namespace "1.2.0"]
+   [org.clojure/tools.namespace "1.4.4"]
    [org.clojure/data.json "2.4.0"]
    [org.clojure/java.classpath "1.0.0"]
-   [org.clojure/core.async "1.5.648" :exclusions [org.clojure/tools.reader]]
+   [org.clojure/core.async "1.6.673" :exclusions [org.clojure/tools.reader]]
    [org.clojure/test.check "1.1.1" :scope "test"]
    [org.elasticsearch.client/transport "7.0.0"]
    [org.elasticsearch.test/framework "7.0.0"
     :exclusions [com.carrotsearch.randomizedtesting/randomizedtesting-runner]]
 
-   [org.slf4j/slf4j-api "1.7.36"]
-   [org.slf4j/slf4j-log4j12 "1.7.36"]
-   [org.slf4j/slf4j-simple "1.7.36"]
+   [org.slf4j/slf4j-api "2.0.7"]
+   [org.slf4j/slf4j-log4j12 "2.0.7"]
+   [org.slf4j/slf4j-simple "2.0.7"]
 
-   [org.apache.curator/curator-test "5.2.1" :scope "test"]
+   [org.apache.curator/curator-test "5.5.0" :scope "test"]
 
-   [ring "1.9.5"]
-   [ring/ring-core "1.9.5" :exclusions [commons-io]]        ; clj-http needs a more recent version
+   [ring "1.10.0"]
+   [ring/ring-core "1.10.0" :exclusions [commons-io]]        ; clj-http needs a more recent version
    [ring/ring-codec "1.2.0"]
    [ring/ring-json "0.5.1"]
-   [ring/ring-defaults "0.3.3"]
+   [ring/ring-defaults "0.3.4"]
 
-   [zookeeper-clj "0.9.4" :exclusions [[org.slf4j/slf4j-log4j12]]]
+   [zookeeper-clj "0.10.0" :exclusions [[org.slf4j/slf4j-log4j12]]]
 
-   [org.apache.zookeeper/zookeeper "3.8.0"
+   [org.apache.zookeeper/zookeeper "3.8.1"
     :exclusions [ch.qos.logback/logback-classic
                  ch.qos.logback/logback-core
                  io.netty/netty-handler
@@ -135,7 +135,7 @@
    [clojure-complete "0.2.5" :scope "test"
     :exclusions [org.clojure/clojure]]
 
-   [com.google.javascript/closure-compiler-unshaded "v20220301"]
+   [com.google.javascript/closure-compiler-unshaded "v20230502"]
 
    ;; cljs testing; control options here
    [doo "0.1.11" :scope "test"]
